cafb15658bb0f36c1cac62e34e274d1500eabed0
Pjkishore2003 <142987411+Pjkishore2003@users.noreply.github.com>
2025-08-24 03:56:53 +0530

    Delete labsession2 directory

--- labsession2/IShape.java
+++
@@ -1,6 +0,0 @@
-package labsession250725;
-
-public interface IShape {
-	double getArea();
-
-}
--- labsession2/circleshape.java
+++
@@ -1,6 +0,0 @@
-package labsession250725;
-
-public interface circleshape extends shape {
-	void radius();
-
-}
